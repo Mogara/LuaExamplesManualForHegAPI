821adc48d69e1d4814fb3734c2481535cb3fa457
Amira <lten@outlook.com>
2015-07-04 12:16:27 +0800

    新增 安恤、暗箭

--- ChapterA.lua
+++ ChapterA.lua
@@ -1,8 +1,110 @@
 --[[
 	国战技能速查手册（A区）
 	技能索引：
-	傲才、
+	暗箭、安恤、傲才、
 ]]--
+
+--[[
+	暗箭
+	相关武将：身份-潘璋＆马忠
+	描述：当你使用【杀】对目标角色造成伤害时，若你不在其攻击范围内，你令伤害值＋1。
+	引用：
+	状态：1.2.0 测试通过
+]]
+
+LuaAnjian = sgs.CreateTriggerSkill{
+	name = "LuaAnjian",
+	can_preshow = true,
+	frequency = sgs.Skill_Compulsory,
+	events = sgs.DamageCaused,
+	
+	can_trigger = function(self, event, room, player, data)
+		if player and player:isAlive() and player:hasSkill(self:objectName()) then
+			local damage = data:toDamage()
+			if damage.chain or damage.transfer or not damage.by_user then return "" end
+			if damage.card and damage.card:isKindOf("Slash") then
+				if damage.from and not damage.to:inMyAttackRange(damage.from) then
+					return self:objectName()
+				end
+			end
+		end
+		return ""
+	end,
+	
+	on_cost = function(self, event, room, player, data)
+		if player:hasShownSkill(self:objectName()) or player:askForSkillInvoke(self:objectName(), data) then
+			room:broadcastSkillInvoke(self:objectName(), player)
+			return true 
+		end
+		return false 
+	end,
+	
+	on_effect = function(self, event, room, player, data)
+		room:sendCompulsoryTriggerLog(player, self:objectName(), true)
+		local damage = data:toDamage()
+		damage.damage = damage.damage + 1
+		data:setValue(damage)
+		return false 
+	end,
+}
+
+--[[
+	安恤
+	相关武将：身份-步练师
+	描述：出牌阶段限一次，你可选择两名手牌数不同的其他角色，令其中手牌少的角色先获得手牌多的角色的一张手牌再展 示之，然后若以此法展示的牌不为♠，你摸一张牌。 
+	引用：
+	状态：1.2.0 测试通过
+]]
+
+LuaAnxuCard = sgs.CreateSkillCard{
+	name = "LuaAnxuCard",
+	skill_name = "LuaAnxu",
+	target_fixed = false,
+	will_throw = false,
+	handling_method = sgs.Card_MethodNone,
+
+	filter = function(self, targets, to_select, player)
+		if to_select:objectName() == player:objectName() then
+			return false
+		elseif #targets == 0 then
+			return true
+		elseif #targets == 1 then 
+			return to_select:objectName() ~= targets[1]:objectName() and to_select:getHandcardNum() ~= targets[1]:getHandcardNum()
+		end
+		return false
+	end,
+	
+	feasible = function(self, targets)
+		return #targets == 2
+	end,
+
+	on_use = function(self, room, source, targets)
+		local from = targets[1]:getHandcardNum() < targets[2]:getHandcardNum() and targets[1] or targets[2]
+		local to = from:objectName() == targets[1]:objectName() and targets[2] or targets[1]
+		local id = room:askForCardChosen(from, to, "h", self:getSkillName(), false)
+		local card = sgs.Sanguosha:getCard(id)
+		from:obtainCard(card)
+		room:showCard(from, id)
+		if card:getSuit() ~= sgs.Card_Spade then
+			room:drawCards(source, 1, self:getSkillName())
+		end
+	end,
+}
+
+LuaAnxu = sgs.CreateZeroCardViewAsSkill{   
+	name = "LuaAnxu",
+	
+	view_as = function(self)
+		local skillcard = LuaAnxuCard:clone()
+		skillcard:setSkillName(self:objectName())
+		skillcard:setShowSkill(self:objectName())
+		return skillcard
+	end,
+
+	enabled_at_play = function(self, player)
+		return not player:hasUsed("#LuaAnxuCard")
+	end,
+}
 
 --[[
 	傲才
