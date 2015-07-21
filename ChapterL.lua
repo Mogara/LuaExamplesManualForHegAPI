5ed9fcac2f4926bb6ca6b6392d161455553ac083
Amira <lten@outlook.com>
2015-07-22 01:16:12 +0800

    调整 落英
    插入中同类型技能选择时一个小问题

--- ChapterL.lua
+++ ChapterL.lua
@@ -918,6 +918,7 @@ LuaLuoying = sgs.CreateTriggerSkill{
 
 			if move.to_place == sgs.Player_DiscardPile and (jud or (dis and move.from_places:contains(sgs.Player_PlaceTable))) then	
 				for _, idstring in ipairs(luoyingCards_string:split("+")) do
+					if not move.card_ids:contains(tonumber(idstring)) then return "" end			
 					if room:getCardPlace(tonumber(idstring)) == sgs.Player_DiscardPile then table.insert(luoying_ids, idstring) end
 				end
 				room:setPlayerProperty(player, "luoying_toget", sgs.QVariant(table.concat(luoying_ids, "+")))
